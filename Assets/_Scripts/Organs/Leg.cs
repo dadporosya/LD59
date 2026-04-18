294e669cde00248aa7987ca6d93b0b3f565d2e64
dadporosya <ilja.amogus@gmail.com>
2026-04-18 15:26:58 +0300

    leg test

--- Assets/_Scripts/Organs/Leg.cs
+++ Assets/_Scripts/Organs/Leg.cs
@@ -4,16 +4,20 @@ public class Leg : OrganBase
 {
     public int XPPerStep = 5;
     private EscapeProgressManager escapeProgressManager;
+    private Animator animator;
     
     public override void Awake()
     {
         base.Awake();
-        if (!escapeProgressManager) escapeProgressManager = FindFirstObjectByType<EscapeProgressManager>()
+        if (!escapeProgressManager) escapeProgressManager = FindFirstObjectByType<EscapeProgressManager>();
+        if (!animator) animator = GetComponent<Animator>();
     }
 
     public override void Action()
     {
+        base.Action();
         escapeProgressManager.ChangeXP(XPPerStep);
+        animator.Play("LegStep");
     }
     
 }
