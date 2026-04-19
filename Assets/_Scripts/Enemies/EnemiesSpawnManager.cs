2fc38dc57dfeaf36ec88958ee6394c50b9c0af71
dadporosya <ilja.amogus@gmail.com>
2026-04-19 20:17:56 +0300

    enemy spawner base

--- Assets/_Scripts/Enemies/EnemiesSpawnManager.cs
+++ Assets/_Scripts/Enemies/EnemiesSpawnManager.cs
@@ -5,26 +5,60 @@ public class EnemiesSpawnManager : MonoBehaviour
 {
     public GameObjectObjectsKindsContainer enemyPrefabs;
     private EscapeProgressManager escapeProgressManager;
+    
 
     public int threatLevel = 1;
 
     public Transform spawnPoint;
+
+    [SerializeField] private float baseXpValue = 20f;
+    [SerializeField] private float stepXP = 5f;
+    public float XPPerEnemy=20f;// ?
+
+    public float XPQuota=0;
+    public float gainedXP = 0;
     private void Start()
     {
         escapeProgressManager = FindFirstObjectByType<EscapeProgressManager>();
+        UpdateThreatLevel();
     }
 
+    public void GenerateQuota()
+    {
+        XPQuota = h.RangeWithCoof(XPPerEnemy, 0.6f);
+    }
+    public void ProcessXP(float XP)
+    {
+        gainedXP += XP;
+        UpdateThreatLevel();
+        if (gainedXP >= XPQuota)
+        {
+            gainedXP = h.Min(0, gainedXP - XPQuota);
+            SpawnEnemy();
+            GenerateQuota();
+            ProcessXP(0);
+        }
+    }
+    
     public void UpdateThreatLevel()
     {
         threatLevel = (int)escapeProgressManager.currentXP / 100 + 1;
-        
+        XPPerEnemy = baseXpValue + stepXP * (threatLevel - 1);
         // update enemies mb
     }
 
     public GameObject SpawnEnemy(GameObject enemyPrefab = null)
     {
         if (!enemyPrefab) enemyPrefab = h.RandChoice(enemyPrefabs.objects.Values.ToList());
+        GameObject enemy = null;
+        if (enemyPrefab)
+            enemy = Instantiate(
+                enemyPrefab, 
+                spawnPoint.position,
+                enemyPrefab.transform.rotation,
+                GameObject.FindGameObjectWithTag("ScrollingParent").transform);
         
+        return enemy;
     }
     
 }
