52f02fb25e2b4fdb1b48be170dd0d1d4aa410496
dadporosya <ilja.amogus@gmail.com>
2026-04-21 02:22:54 +0300

    pobeda

--- Assets/_Scripts/CutScenesScripts/FinalMouthCutScene.cs
+++ Assets/_Scripts/CutScenesScripts/FinalMouthCutScene.cs
@@ -2,16 +2,22 @@ using System;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
+using UnityEngine.UI;
 
 public class FinalMouthCutscene : CutSceneBase
 {
     [SerializeField] float gapBetweenWords = 0.4f;
+    private Mouth mouth;
+    private GameObject finalCanvas;
+    [SerializeField] private float translateDuration=3f;
     public override void Init()
     {
         base.Init();
         List<IEnumerator> rawSteps = new List<IEnumerator>()
         {
+            TranslateMouthToCenter(translateDuration),
             CutsceneStep(),
         };
         
@@ -21,11 +27,61 @@ public class FinalMouthCutscene : CutSceneBase
         }
     }
 
+    public IEnumerator TranslateMouthToCenter(float duration = 1f)
+    {
+        mouth = FindFirstObjectByType<Mouth>();
+        finalCanvas =  GameObject.FindGameObjectWithTag("FinalCanvas");
+        Image image =  finalCanvas.GetComponentInChildren<Image>();
+        
+        // Target values
+        Vector3 screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 10f);
+        Vector3 targetPosition = Camera.main.ScreenToWorldPoint(screenCenter);
+        Vector3 targetScale = Vector3.one;
+        
+        // Starting values
+        Vector3 startPosition = mouth.transform.position;
+        Vector3 startScale = mouth.transform.localScale;
+        
+        h.FadeIn(image.gameObject, duration, this);
+        
+        // Smoothly animate to target
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / duration);
+            
+            mouth.transform.position = Vector3.Lerp(startPosition, targetPosition, t);
+            mouth.transform.localScale = Vector3.Lerp(startScale, targetScale, t);
+            
+            yield return null;
+        }
+        
+        // Ensure final values are exact
+        mouth.transform.position = targetPosition;
+        mouth.transform.localScale = targetScale;
+        
+        yield return new  WaitForSeconds(0.5f);
+    }
     public IEnumerator CutsceneStep()
     {
-        Mouth mouth = FindFirstObjectByType<Mouth>();
-        
-        
+        List<string> phrase = new List<string>()
+        {
+            "Thanks", " For", " Playing"
+        };
+        if (!mouth) mouth = FindFirstObjectByType<Mouth>();
+        if (!finalCanvas) finalCanvas =  GameObject.FindGameObjectWithTag("FinalCanvas");
+        TextMeshProUGUI text = finalCanvas.GetComponentInChildren<TextMeshProUGUI>();
+        text.text = "";
+
+        gapBetweenWords = h.Max(mouth.gapBetweenFrames, gapBetweenWords);
+
+        foreach (var word in phrase)
+        {
+            text.text += word;
+            mouth.Talk();
+            yield return new WaitForSeconds(gapBetweenWords);
+        }
         
         yield return null;
     }
